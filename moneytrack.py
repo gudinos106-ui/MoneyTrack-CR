079a4477948c6327534a26691ef1ae60458e3894
gudinos106-ui <gudinos106@gmail.com>
2026-05-14 16:34:41 -0600

    Update moneytrack.py

--- moneytrack.py
+++ moneytrack.py
@@ -15,7 +15,7 @@ from utils import enviar_telegram, limpiar_monto, categorizar
 
 st.set_page_config(
     page_title="MoneyTrack Gold",
-    page_icon="https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/logo.png",
+    page_icon="https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/icono_gold.png",
     layout="wide"
 )
 def configurar_pwa():
