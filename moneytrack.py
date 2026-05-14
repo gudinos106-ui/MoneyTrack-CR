0d7b4cd599f131b55fa89754506ab78ae4b26422
gudinos106-ui <gudinos106@gmail.com>
2026-05-14 12:28:07 -0600

    Update moneytrack.py

--- moneytrack.py
+++ moneytrack.py
@@ -19,7 +19,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 def configurar_pwa():
-    manifest_url = "https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/manifest.json"
+    manifest_url = "https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/manifest.json?v=2"
     sw_url = "https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/sw.js"
     
     pwa_html = f"""
