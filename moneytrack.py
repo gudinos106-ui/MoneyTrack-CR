9615f95477d07ee9a00da9820699e6b299d220d7
gudinos106-ui <gudinos106@gmail.com>
2026-05-15 10:11:05 -0600

    Restore imports and PWA configuration

--- moneytrack.py
+++ moneytrack.py
@@ -15,7 +15,7 @@ from utils import enviar_telegram, limpiar_monto, categorizar
 
 st.set_page_config(
     page_title="MoneyTrack Gold",
-    page_icon="https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/Money_Gold.png",
+    page_icon="https://raw.githubusercontent.com/gudinos106-ui/MoneyTrack-CR/refs/heads/main/money.png",
     layout="wide"
 )
 def configurar_pwa():
